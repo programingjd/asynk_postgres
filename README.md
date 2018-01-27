7a0bceb6ceb7a794f09fbf6b0960c0fba7e7d07d
Jerome David <programingjd@gmail.com>
2018-01-27 15:52:19 +0100

    -

---
+++ README.md
@@ -0,0 +1,35 @@
+![jcenter](https://img.shields.io/badge/_jcenter_-1.0.0-6688ff.png?style=flat) &#x2003; ![jcenter](https://img.shields.io/badge/_Tests_-28/28-green.png?style=flat)
+# server
+A Postgres async client with suspend functions for kotlin coroutines.
+
+## Download ##
+
+The maven artifacts are on [Bintray](https://bintray.com/programingjd/maven/info.jdavid.postgres/view)
+and [jcenter](https://bintray.com/search?query=info.jdavid.postgres).
+
+[Download](https://bintray.com/artifact/download/programingjd/maven/info/jdavid/postgres/postgres/1.0.0/postgres-1.0.0.jar) the latest jar.
+
+__Maven__
+
+Include [those settings](https://bintray.com/repo/downloadMavenRepoSettingsFile/downloadSettings?repoPath=%2Fbintray%2Fjcenter)
+ to be able to resolve jcenter artifacts.
+```
+<dependency>
+  <groupId>info.jdavid.postgres</groupId>
+  <artifactId>postgres</artifactId>
+  <version>1.0.0</version>
+</dependency>
+```
+__Gradle__
+
+Add jcenter to the list of maven repositories.
+```
+repositories {
+  jcenter()
+}
+```
+```
+dependencies {
+  compile 'info.jdavid.postgres:postgres:1.0.0'
+}
+```
---
+++ build.gradle.kts
@@ -0,0 +1,101 @@
+import com.jfrog.bintray.gradle.BintrayExtension
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+  repositories {
+    jcenter()
+  }
+}
+
+plugins {
+  kotlin("jvm") version "1.2.21"
+  `maven-publish`
+  id("com.jfrog.bintray") version "1.7.3"
+}
+
+group = "info.jdavid.postgres"
+version = "1.0.0"
+
+repositories {
+  jcenter()
+}
+
+dependencies {
+  compile(kotlin("stdlib-jdk8"))
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.22")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-nio:0.22")
+  implementation("org.slf4j:slf4j-api:1.7.25")
+  testImplementation("junit:junit:4.12")
+  testRuntime("org.slf4j:slf4j-jdk14:1.7.25")
+}
+
+kotlin {
+  experimental.coroutines = Coroutines.ENABLE
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+  classifier = "sources"
+  from(java.sourceSets["main"].allSource)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+  classifier = "javadoc"
+}
+
+tasks.withType(KotlinJvmCompile::class.java).all {
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+}
+
+val jar: Jar by tasks
+jar.apply {
+  manifest {
+    attributes["Sealed"] = true
+  }
+}
+
+publishing {
+  repositories {
+    maven {
+      url = uri("${buildDir}/repo")
+    }
+  }
+  (publications) {
+    "mavenJava"(MavenPublication::class) {
+      from(components["java"])
+      artifact(sourcesJar)
+      artifact(javadocJar)
+    }
+  }
+}
+
+bintray {
+  user = "programingjd"
+  key = { "" }()
+  dryRun = true
+  publish = true
+  setPublications("mavenJava")
+  pkg(delegateClosureOf<BintrayExtension.PackageConfig>{
+    repo = "maven"
+    name = "${project.group}"
+    websiteUrl = "https://github.com/programingjd/postgres"
+    issueTrackerUrl = "https://github.com/programingjd/postgres/issues"
+    vcsUrl = "https://github.com/programingjd/postgres.git"
+    githubRepo = "programingjd/postgres"
+    githubReleaseNotesFile = "README.md"
+    setLicenses("Apache-2.0")
+    setLabels("postgres", "postgresql", "sql", "java", "kotlin", "async", "coroutines")
+    publicDownloadNumbers = true
+    version(delegateClosureOf<BintrayExtension.VersionConfig> {
+      name = "${project.version}"
+      mavenCentralSync(delegateClosureOf<BintrayExtension.MavenCentralSyncConfig> {
+        sync = false
+      })
+    })
+  })
+}
+
