2bb43b815744aa6d767167c2c64ea7f4742104b2
Jerome David <programingjd@gmail.com>
2018-06-15 13:48:23 +0200

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,13 @@ buildscript {
 }
 
 plugins {
-  kotlin("jvm") version "1.2.41"
+  kotlin("jvm") version "1.2.50"
   `maven-publish`
-  id("com.jfrog.bintray") version "1.8.0"
+  id("com.jfrog.bintray") version "1.8.1"
 }
 
 group = "info.jdavid.asynk"
-version = "0.0.0.1"
+version = "0.0.0.2"
 
 repositories {
   jcenter()
@@ -33,12 +33,12 @@ repositories {
 
 dependencies {
   compile(kotlin("stdlib-jdk8"))
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.22.5")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-nio:0.22.5")
-  implementation("info.jdavid.asynk:sql:0.0.0.1")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.23.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-nio:0.23.2")
+  implementation("info.jdavid.asynk:sql:0.0.0.2")
   implementation("org.slf4j:slf4j-api:1.7.25")
   testImplementation("junit:junit:4.12")
-  testImplementation("com.fasterxml.jackson.core:jackson-databind:2.9.5")
+  testImplementation("com.fasterxml.jackson.core:jackson-databind:2.9.6")
   testRuntime("org.slf4j:slf4j-jdk14:1.7.25")
 }
 
