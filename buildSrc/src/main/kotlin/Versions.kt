88c70267b8016801ab9d0ab38c62e2763cc0370f
Jerome David <programingjd@gmail.com>
2019-06-19 18:00:22 +0200

    -

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 object ASYNK {
-  val version = "0.0.0.31"
+  val version = "0.0.0.32"
 }
 object KOTLIN {
-  val version = "1.3.31"
+  val version = "1.3.40"
 }
 object KOTLINX {
-  val version = "1.2.1"
+  val version = "1.2.2"
 }
